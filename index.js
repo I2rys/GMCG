1a1271e280fddd489ed9459f0a6db122bdbb9065
Trac3D1y <i2rys@protonmail.com>
2021-10-27 12:21:42 +0800

    Removed useless stuff

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const Self_Args = process.argv.slice(2)
 var results = ""
 
 //Main
-if(Self_Args.length == 0){
+if(!Self_Args.length){
     console.log(`node index.js <amount> <output>
 Example: node index.js 100 ./output_test.txt`)
     process.exit()
 }
 
-if(Self_Args[0] == ""){
+if(!Self_Args[0]){
     console.log("Invalid amount.")
     process.exit()
 }
@@ -24,7 +24,7 @@ if(isNaN(Self_Args[0])){
     process.exit()
 }
 
-if(Self_Args[1] == ""){
+if(!Self_Args[1]){
     console.log("Invalid output.")
     process.exit()
 }
@@ -45,7 +45,7 @@ for( i = 0; i <= Self_Args[0]; i++ ){
         charset: "gwd"
     })
 
-    if(results.length == 0){
+    if(!results.length){
         results = `${fragment_1}-${fragment_2}-${fragment_3}`
     }else{
         results += `\n${fragment_1}-${fragment_2}-${fragment_3}`
